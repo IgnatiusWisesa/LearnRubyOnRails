085aa086eff44c1393244793602096700b8122a8
Ignatius Wisesa <wisesa.dwi@gmail.com>
2025-08-26 14:15:17 +0700

    allow /up for healthcheck; whitelist IP host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,13 +24,14 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.hosts << "3.106.145.17"
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = false
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = false
-
-  config.hosts << "3.106.145.17"
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
